68482694522798d48188f5bbb18bacd6649b413e
danifgx <danifgx@gmail.com>
2023-09-05 21:41:06 +0200

    Added login with google from front-end

---
+++ acortadirecciones-front/src/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Center, VStack } from '@chakra-ui/react';
+
+const Login = () => {
+
+  const handleGoogleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  };
+
+  return (
+    <Center h="100vh" bg="gray.100">
+      <VStack spacing={4}>
+        <Button
+          onClick={handleGoogleLogin}
+          colorScheme="blue"
+          size="lg"
+        >
+          Login with Google
+        </Button>
+      </VStack>
+    </Center>
+  );
+};
+
+export default Login;
